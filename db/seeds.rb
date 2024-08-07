3556879e849d9148a1c21bbeffe3bbf09e31e88e
Chaewan Shin <chaewanshin@gmail.com>
2024-08-07 23:01:37 +0900

    file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name, category: Restaurant::CATEGORIES.sample, address: Faker::Address.street_address)
+end
+
+Restaurant.all.each do |restaurant|
+  5.times do
+    Review.create!(
+      content: Faker::Restaurant.review,
+      rating: rand(1..5),
+      restaurant: restaurant
+    )
+  end
+end
